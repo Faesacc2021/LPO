e5caf5cc1a00258dfd58c1f13ba18fd2851d6e3a
Alunos Laboratorio <aluno.laboratorio@faesa.br>
2022-08-03 21:46:50 -0300

    Projeto Avaliação LPO

---
+++ AvaliacaoLPO/src/Vendedor.java
@@ -0,0 +1,70 @@
+
+public class Vendedor {
+	
+	private String nome;
+	private double salarioBase;
+	private int tempoServico;
+	private char  categoria;
+	private double totalVendas;
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getSalarioBase() {
+		return salarioBase;
+	}
+	public void setSalarioBase(double salarioBase) {
+		this.salarioBase = salarioBase;
+	}
+
+	public int getTempoServico() {
+		return tempoServico;
+	}
+	public void setTempoServico(int tempoServico) {
+		this.tempoServico = tempoServico;
+	}
+
+	public char getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(char categoria) {
+		this.categoria = categoria;
+	}
+
+	public double getTotalVendas() {
+		return totalVendas;
+	}
+	public void setTotalVendas(double totalVendas) {
+		this.totalVendas = totalVendas;
+	}
+	@Override
+	public String toString() {
+		return "Vendedor [nome=" + nome + ", salarioBase=" + salarioBase
+				+ ", tempoServico=" + tempoServico + ", categoria=" + categoria
+				+ ", totalVendas=" + totalVendas + "]";
+	}
+	
+    public static void incializaVendedores() {
+        Vendedor vendedor = new Vendedor();
+        vendedor.setSalarioBase(1500.00);
+    }
+    
+    public static void defineCategoria(Vendedor vendedor, int tempoServico) {
+    	if (tempoServico > 60) {
+    		vendedor.setCategoria('A'); 
+    	} else if (tempoServico > 30) {
+    		vendedor.setCategoria('B');
+    	}  else {
+    		vendedor.setCategoria('C');
+    	}
+    }
+}
+
+
+
+
+
---
+++ AvaliacaoLPO/src/VetVendedor.java
@@ -0,0 +1,17 @@
+
+public class VetVendedor {
+	private Vendedor[] vetVendedor;
+	private int countVend;
+	
+	
+	definir tamanho do vetor
+	getvendedor pos
+	getcount
+	pesquisa
+	insere
+	remove
+	totalvendas
+	quantvendedor
+	mediavendas
+	tiraespaco
+}
