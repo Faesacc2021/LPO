5b98e73cac96a12da95de48f12cc08a091ee0b97
Alunos Laboratorio <aluno.laboratorio@faesa.br>
2022-09-21 21:38:09 -0300

    Aula do dia 21.09.2022

---
+++ AvaliacaoLPO/src/salaDeAula/Aluno.java
@@ -0,0 +1,45 @@
+package salaDeAula;
+
+public class Aluno extends Pessoa  {
+	private int matricula;
+	private String curso;
+	
+	public Aluno(String nome, String cpf, int matricula, String curso) {
+		super(nome, cpf);
+		this.matricula = matricula;
+		this.curso = curso;
+	}
+
+	public Aluno(String nome, String cpf, Endereco endereco, int matricula,
+			String curso) {
+		super(nome, cpf, endereco);
+		this.matricula = matricula;
+		this.curso = curso;
+	}
+	
+	public Aluno() {
+		super();
+		this.matricula = 0;
+		this.curso = "";
+	}
+	
+	public int getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+
+	@Override
+	public String toString() {
+		return "Aluno [matricula=" + matricula + ", curso=" + curso + ", nome="
+				+ nome + ", cpf=" + cpf + ", endereco=" + endereco + "]";
+	}
+}
---
+++ AvaliacaoLPO/src/salaDeAula/Pessoa.java
@@ -0,0 +1,51 @@
+package salaDeAula;
+
+public class Pessoa {
+	String nome;
+	String cpf;
+	Endereco endereco;
+	
+	public Pessoa(String nome, String cpf, Endereco endereco) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.endereco = endereco;
+	}
+
+	public Pessoa() {
+		this.nome = "";
+		this.cpf = "";
+		this.endereco = null;
+	}
+
+	public Pessoa(String nome, String cpf) {
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	@Override
+	public String toString() {
+		return "Pessoa [nome=" + nome + ", cpf=" + cpf + ", endereco="
+				+ endereco + "]";
+	}
+}
